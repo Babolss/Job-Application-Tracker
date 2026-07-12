b08281bf7fced6ef38f513aa6d48844c80ae147a
Babolss <alaindavidsondeleon@gmail.com>
2026-07-12 13:25:27 +0800

    downgrade to prisma 6 for stability

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,11 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require(".prisma/client/default");
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: unknown;
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
